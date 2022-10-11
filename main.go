7d8da10b7696704070655d1d35f6b02270b85290
ksh3490 <ksh349021@gmail.com>
2022-10-11 23:22:52 +0900

    feat: Create main.go to start to write source codes.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+}
+
+var ErrNoPath = errors.New("Path Required")
+
+func execInput(input string) error {
+
+}
