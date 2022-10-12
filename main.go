b99d2a29af9eee32e11fa4485da376ccbceab971
ksh3490 <ksh349021@gmail.com>
2022-10-12 12:25:04 +0900

    feat: Create basic shell that can't recognize input's arguments

--- main.go
+++ main.go
@@ -3,15 +3,42 @@ package main
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(">> ")
+		// Read the keyboard input
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		// Handle the execution of the input
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
 }
 
 var ErrNoPath = errors.New("Path Required")
 
 func execInput(input string) error {
+	// Remove the new line character
+	input = strings.TrimSuffix(input, "\n")
 
+	// Prepare the command to execute
+	cmd := exec.Command(input)
+
+	// Set the correct device
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	// Execute the command and return the error
+	return cmd.Run()
 }
